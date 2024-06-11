2b18e41d5329eeb6415fef6a0449869097e7b83c
tsaren99 <tsaren99@sam-solutions.net>
2024-06-11 13:23:11 +0300

    add axios

--- ui-service/src/component/AudioListView.jsx
+++ ui-service/src/component/AudioListView.jsx
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom";
+import axios from 'axios'
+import React, { useEffect, useState } from 'react';
 
 function AudioListView() {
-  
+
+    let list = {
+        field : ""
+    }
+
+    const [s,setS] = useState();
+
+
+
+    useEffect(() => {
+        getList()
+    }, )
+
+
     return (
+
         <div className={"wrapper"}>
             This is audio page
             <Link to={".."}>to main view</Link>
+            <p>{s}</p>
         </div>
     )
 }
--- ui-service/src/index.js
+++ ui-service/src/index.js
@@ -27,5 +27,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-reportWebVitals();
+registerServiceWorker()
